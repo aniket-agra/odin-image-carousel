90974f9c93a845508dc147dde99bc3d7dc8dbbeb
Aniket Agrawal <agra.aniket@gmail.com>
2023-05-12 18:23:08 +0200

    Create display div to serve as the frame
    We need to cycle images through the display div

--- src/carousel.js
+++ src/carousel.js
@@ -5,12 +5,17 @@ let bodyElem = document.querySelector("body");
 let carouselElem = document.createElement("div");
 carouselElem.classList.add("carousel");
 
+let displayElem = document.createElement("div");
+displayElem.classList.add("display");
+
 for (let i = 0; i < 10; i++) {
     let image = document.createElement("img");
     image.classList.add("picture");
     image.setAttribute("src", "");
     image.setAttribute("alt", `Img${i}`);
-    carouselElem.appendChild(image);
+    i === 0 ? displayElem.appendChild(image) : carouselElem.appendChild(image);
 }
+
+carouselElem.appendChild(displayElem);
 
 bodyElem.appendChild(carouselElem);
